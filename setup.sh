332fc4258dbe09fb82082363b5a83e87ec3ce7db
YATHIN <yathinprakash017@gmail.com>
2021-08-10 18:29:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,16 @@ echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit/
+
+echo "[theme]
+primaryColor = '#ff8500'
+backgroundColor = '#f9d7b1'
+secondaryBackgroundColor = '#e9af6f'
+textColor= '#000000'
+font = 'sans serif'
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
